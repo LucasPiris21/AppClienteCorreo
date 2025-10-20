428376f475f9e75f7796740fce503cfc0dfaf46c
Miguel Nicolás Basualdo Martinez <niicomartiinez22@gmail.com>
2025-10-20 00:20:24 -0300

    Cambios al front

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -19,3 +19,24 @@ document.getElementById("altaClientesForm").onsubmit = function(event) {
 		})
 	};
 
+
+// Ocultar modal
+const modal = document.getElementById('modal');
+const openBtn = document.getElementById('openFormBtn');
+const closeBtn = document.getElementById('closeModal');
+
+openBtn.addEventListener('click', () => {
+  modal.classList.add('show');
+});
+
+closeBtn.addEventListener('click', () => {
+  modal.classList.remove('show');
+});
+
+window.addEventListener('click', (e) => {
+  if(e.target === modal) {
+    modal.classList.remove('show');
+  }
+});
+
+
